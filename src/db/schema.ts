f536d64c314aca45e488feda8803fe252751a67a
maanavbhavsar <maanavbhavsar@gmail.com>
2025-09-13 14:40:58 -0700

    Database

---
+++ src/db/schema.ts
@@ -0,0 +1,13 @@
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  age: integer().notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+});
+
+// Add this at the bottom:
+export const allTables = {
+  usersTable,
+};
